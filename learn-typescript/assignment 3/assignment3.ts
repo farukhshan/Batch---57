f0f69b5fffa98bdb16204b6b458586a4e92e2e07
farrukh shan <farrukhshan55@gmail.com>
2024-04-15 00:03:39 +0500

    Assignment 3 ans 4

---
+++ learn-typescript/assignment 3/assignment3.ts
@@ -0,0 +1,55 @@
+// task 1: Declare a variable for storing your favorite movie name with a string type annotation.
+let movieName: string = "jumanji";
+
+//task 2: Create a template string to introduce yourself, including your name and age
+let myName: string = "farrukh shan";
+let age1 = 25;
+console.log(`hello, my name is`, myName, `and i am `, age1, `years old`);
+
+//task3: Initialize a constant with a type annotation for your current mood as a boolean.
+const myCurrentmoode: Boolean = true;
+console.log(`my current moode with a type as a boolean is`, myCurrentmoode);
+
+//task 4: Combine a user's first and last name using template literals and log the full name.
+const firsyName: string = "farrukh";
+const lastName: string = "shan";
+const fullName: string = `${firsyName} ${lastName}`;
+console.log(`my fullname is`, fullName);
+
+//task 5: Create a variable for a user's age with a type annotation and assign a number to it.
+const userAge = 25;
+console.log(`user age is`, userAge);
+
+//task 6: Use a template string to display a product name and its price.
+const productName: string = "surf excel";
+const productPrice: string = "200 rupees";
+console.log(
+  `product name is`,
+  productName,
+  `and the price of product is`,
+  productPrice
+);
+
+//task 7:Declare a variable to store the number of pets you have with a number type annotation
+const numOfPets: number = 10;
+console.log(`I have a pet in quantity is `, numOfPets);
+
+//task 8:Declare a variable with a boolean type annotation indicating if it is your birthday today.
+const myBirthday: boolean = true;
+console.log(`Is it my birthday today`, myBirthday);
+
+//task 9 :Create a template string that includes a user's first name, last name, and age with proper labels.
+const properLabel: string = `first name: ${firsyName} , last name: ${lastName}, age: ${userAge}`;
+
+//task10: Initialize a constant to hold your birth year with a number type annotation.
+const myBirthyear: number = 1997;
+console.log(`My birth year is`, myBirthday);
+
+//task11: Write a line of code that uses a template string to combine a city and country into a full address.
+let city: string = "lahore";
+let country: string = "pakistan";
+const fulladdress: string = `city: ${city} country: ${country}`;
+
+//task 12: Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let todayTemp: number = 35;
+console.log(`Today tempeture is`, todayTemp);
---
+++ learn-typescript/assignment 4/assignment4.ts
@@ -0,0 +1,139 @@
+//Task 1: Assign "Hello, World!" to this variable and print it.
+let greeting: string = "Hello , world!";
+console.log(greeting);
+
+//task2: Assign integer values and perform arithmetic operations.
+let num1 = 5;
+let num2 = 10;
+console.log(`Calculation of sum ${num1 + num2}`);
+console.log(`Calculation of Substraction ${num1 - num2}`);
+console.log(`Calculation of Division ${num1 / num2}`);
+console.log(`Calculation of Multiplication ${num1 * num2}`);
+
+//task3: Swap these values without using a new variable.
+let a = 1;
+let b = 2;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("a=", a);
+console.log("b=", b);
+
+//task 4: let message: string; // Now try assigning a number to it and see what happens.
+// let message: string = 1; // error occur
+// let message: string = true; // error occur, we cant assign boolean or number to string variable
+
+//task 5: let num1, num2; // Use the modulus operator (%) to find the remainder.
+console.log(`Remainder of num1 or num2 is ${num1 % num2}`);
+
+// task 6: let counter = 0; // Increment this value by 1 in two different ways.
+let counter = 0;
+counter += 1; //first method
+counter++; // second method
+
+// task 7: let a = true, b = false, c = true; // Write expressions using these.
+let a1 = true;
+let b2 = false;
+let c3 = true;
+a1 && b2 && c3; //and expression
+a1 || b2 || c3; //or expression
+// !a1 !b2; !c3; //not expression
+
+// task 8: let num = 10; // Use +=, -=, *=, and /= on this variable.
+let num = 10;
+num += 10;
+console.log(num);
+num -= 10;
+console.log(num);
+num *= 10;
+console.log(num);
+num /= 10;
+console.log(num);
+
+// task 9: let num; // Determine if this is even or odd.
+let number = 10;
+let v = number % 2;
+if (v === 0) {
+  console.log(`${number} number is even`);
+} else {
+  console.log(`${number} number is odd`);
+}
+
+// task 10: let age; // Check if age is 18 or older to determine voting eligibility.
+let age;
+let currentYear = 2024;
+let burthYear = 2004;
+age = currentYear - burthYear;
+if (age >= 18) {
+  console.log(`Congragulation, You're eligible for vote`);
+} else {
+  console.log(`Sorry. you are not eligible for vote`);
+}
+// task 11: let score; // Use conditionals to assign and print grades A, B, C, D, or F.
+let score: number = 45;
+let grade;
+if (score >= 90 || score <= 100) {
+  grade = "A+";
+} else if (score >= 80 || score <= 89) {
+  grade = "A";
+} else if (score >= 79 || score <= 79) {
+  grade = "B";
+} else if (score >= 60 || score <= 69) {
+  grade = "c";
+} else if (score >= 50 || score <= 59) {
+  grade = "C";
+} else {
+  console.log("fail");
+}
+console.log(`the grade is`, grade);
+
+// task 12:let x, y, z; // Determine the largest among these.
+let x = 10;
+let y = 5;
+let z = 3;
+if (x > y && x > z) {
+  console.log("x is greator then others");
+} else if (y > x && y > z) {
+  console.log("y is greator then others");
+} else {
+  console.log("z is greator then others");
+}
+// task 13: let year; // Determine if this is a leap year.
+let year = 2024;
+if (year % 4 == 0) {
+  console.log("2024 year is a leap year");
+} else {
+  console.log("2024 is not a leap year");
+}
+// task 14: let fahrenheit; // Convert this to Celsius and print the result.
+let fahrenheit = 40.1;
+let Celsius;
+Celsius = ((fahrenheit - 32) * 5) / 9;
+console.log(`The conversion of fahrenheit 40.1 into celcius is`, Celsius);
+
+//task 15: let number; // Determine the sign of this number.
+console.log(" nu is any number");
+
+let nu;
+nu = 21;
+if (nu > 0) {
+  console.log(`nu is a positive number , which is `, nu);
+} else if (nu < 0) {
+  console.log(`nu is a negative number , which is `, nu);
+} else {
+  console.log("nu is a neither positibe neither negative");
+}
+
+// task 16: let number; // Print the multiplication table for this number up to 10.
+let nm = 10;
+console.log(`10 * 1 =`, nm * 1);
+console.log(`10 * 2 =`, nm * 2);
+console.log(`10 * 3 =`, nm * 3);
+console.log(`10 * 4 =`, nm * 4);
+console.log(`10 * 5 =`, nm * 5);
+console.log(`10 * 6 =`, nm * 6);
+console.log(`10 * 7 =`, nm * 7);
+console.log(`10 * 8 =`, nm * 8);
+console.log(`10 * 9 =`, nm * 9);
+console.log(`10 * 10 =`, nm * 10);
+console.log("this is the Multiplication table of 10");
